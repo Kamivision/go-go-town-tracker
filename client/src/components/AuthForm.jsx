c6124ee23e42aac7d361d4ffbb3c871944688172
kamivision <kamivision@gmail.com>
2026-06-15 14:36:37 -0500

    created server skeleton

---
+++ client/src/components/AuthForm.jsx
@@ -0,0 +1,7 @@
+export default function AuthForm() {
+    return (
+        <div>
+            <h1>AuthForm</h1>
+        </div>
+    )
+}
---
+++ server/townie_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TownieAppConfig(AppConfig):
+    name = 'townie_app'
